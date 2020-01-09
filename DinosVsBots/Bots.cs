00b330a6ddbb8744370da3180ecdc995b940a0e1
DorianHaley <59707012+DorianHaley@users.noreply.github.com>
2020-01-09 14:44:09 -0600

    initialization

---
+++ DinosVsBots/Bots.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinosVsBots
+{
+    class Bots 
+    {//Member Variables
+
+        string Bot1Name;
+        string Bot2Name;
+        string Bot3Name;
+        string Bot4Name;
+        
+        bool HasHealth;
+        bool HasMana;
+
+        int TrooperHP;
+        int AssaultHP;
+        int ReconHP;
+        int HeavyHP;
+
+        Double TrooperMana;
+        Double AssaultMana;
+        Double ReconMana;
+        Double HeavyMana;
+
+        public Bots() //Constructor
+        {
+            Bot1Name = ("Striker");
+            Bot2Name = ("Shocker");
+            Bot3Name = ("Zero");
+            Bot4Name = ("Berserker");
+
+            TrooperHP = (200);
+            AssaultHP = (300);
+            ReconHP = (250);
+            HeavyHP = (325);
+
+            TrooperMana = (300);
+            AssaultMana = (275);
+            ReconMana = (200);
+            HeavyMana = (150);
+
+            HasHealth = true;
+            HasMana = true;
+        }
+
+        public void ChooseYourBot()//Methods
+        {
+            string Bot1name(int TrooperHP, int TrooperMana);
+            HasMana = true;
+            HasHealth = true;
+            Console.WriteLine("Striker!");
+            Bot1Name = (Console.ReadLine());
+
+           
+            string Bot2name(int AssaultHP, int AssaultMana);
+            Console.WriteLine("Shocker!");
+            Bot2Name = (Console.ReadLine());
+            HasMana = true;
+            HasHealth = true;
+
+
+            string Bot3name(int ReconHP, int ReconMana);
+            Console.WriteLine("Zero!");
+            Bot3Name = (Console.ReadLine());
+            HasHealth = true;
+            HasMana = true;
+
+
+            
+            string Bot4name(int HeavyHP, int HeavyMana);
+            Console.WriteLine("Berzerker!");
+            Bot4Name = (Console.ReadLine());
+            HasMana = true;
+            HasHealth = true;
+
+
+        }
+        
+        
+        
+        
+        
+        
+    }
+}
---
+++ DinosVsBots/Weapons.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinosVsBots
+{
+    public class Weapons //Member Variables
+    {
+        string ChooseWeapon;
+
+        string M4;
+        string Name1;
+
+        string ShotGun;
+        string Name2;
+
+        string Sniper;
+        string Name3;
+
+        string Rocketlaunch;
+        string Name4;
+
+        int Weapon1Damage;
+        int Weapon2Damage;
+        int Weapon3Damage;
+        int Weapon4Damage;
+
+        public Weapons() //Constructor
+        {
+            Name1 = M4;
+            Name2 = ShotGun;
+            Name3 = Sniper;
+            Name4 = Rocketlaunch;
+            Weapon1Damage = 20;
+            Weapon2Damage = 35;
+            Weapon3Damage = 75;
+            Weapon4Damage = 100;
+        }
+        //Methods
+        public void Trooper()
+        {
+            
+        }
+
+        public void Assault()
+        {
+
+        }
+
+        public void Recon()
+        {
+
+        }
+
+        public void Heavy()
+        {
+
+        }
+
+    }
+
+}
