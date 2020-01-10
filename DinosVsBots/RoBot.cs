e7ed63812226e970bd8363cce63589922128e5af
DorianHaley <59707012+DorianHaley@users.noreply.github.com>
2020-01-09 20:43:46 -0600

    Added weapon names and damage levels to the instantiations

--- DinosVsBots/RoBot.cs
+++ DinosVsBots/RoBot.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace DinosVsBots
 {
     public class RoBot
-    {//Member Variables
+    {//Member Variables //name, health, power level, and a Weapon with a type (i.e. sword) and attack power.
 
         string Botname;
         bool HasHealth;
@@ -15,9 +15,12 @@ namespace DinosVsBots
         int BotHP;
         int BotEnergy;
         double BotMana;
+        string BotWeapon;
+        int AttackPower;
+        bool IsActive;
 
                                          //Constructor
-        public RoBot(string BotName, int BotHp, double BotEnergy, bool HasHealth, bool HasEnergy) 
+        public RoBot(string BotName, int BotHp, double BotEnergy, bool HasHealth, bool HasEnergy, bool Isactive, int AttackPower, string Botweaponname) 
         {
                  //Why do these name have to be different from the method signiture?
             BotName = "Name"; 
@@ -25,15 +28,21 @@ namespace DinosVsBots
             BotEnergy = (0);
             HasHealth = true;
             HasEnergy = true;
+            Isactive = true;
+            Botweaponname = null;
+            AttackPower = (0);
+
         }
+
+                              //Methods/If Statements
         public void ChooseAttack()
         {
            
         }
                             //Instantiation
-        RoBot Assault = new RoBot("Striker", 200, 300, true, true);
-        RoBot Shock = new RoBot("Shocker", 250, 250, true, true);
-        RoBot Recon = new RoBot("Shadow", 150, 200, true, true);
-        RoBot Wrecker = new RoBot("Barricade", 350, 150, true, true);  
+        RoBot Assault = new RoBot("Striker", 200, 300, true, true, true, 25, "Rifle");
+        RoBot Shock = new RoBot("Shocker", 250, 250, true, true, true, 35, "Shotgun");
+        RoBot Recon = new RoBot("Shadow", 150, 200, true, true, true, 50, "Sniper");
+        RoBot Wrecker = new RoBot("Barricade", 350, 150, true, true, true, 100, "RocketLauncher");
     }
 }
